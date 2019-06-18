f9107d31f55d62494d901d2c60f0f9d395dab8e3
Takahiro Yamamoto <yamat@icrr.u-tokyo.ac.jp>
2019-06-18 23:46:13 +0900

    update environment variables for m31

--- Fscan/etc/environment.sh
+++ Fscan/etc/environment.sh
@@ -1,4 +1,9 @@
-export KAGRA_FSCANBIN_PATH=/users/DET/tools/Fscan/Script
+if test `whoami` = "controls"
+then
+    export KAGRA_FSCANBIN_PATH=/users/DET/tools/Fscan/Script
+else
+    export KAGRA_FSCANBIN_PATH=${HOME}/opt/Fscan
+fi
 export MAKESFTS_PATH=/usr/bin
 export SPECAVG_PATH=/usr/bin
 export PLOTSPECAVGOUTPUT_PATH=/home/controls/opt/Fscan
