d476f3a976f4b329d7b60dfb03a7e04592791b9d
controls <controls@k1sum1.kagra.icrr.u-tokyo.ac.jp>
2019-07-19 09:56:49 +0900

    Update

--- GlitchPlot/Script/plotter.py
+++ GlitchPlot/Script/plotter.py
@@ -40,20 +40,21 @@ snrthreshold=100.
 #If night, use lower threshold.
 snrdict = {"LSC-CARM_SERVO_MIXER_DAQ_OUT_DQ":10,
            "AOS-TMSX_IR_PD_OUT_DQ":10,
-           "IMC-CAV_TRANS_OUT_DQ":30,
+           "IMC-CAV_TRANS_OUT_DQ":40,
            "IMC-CAV_REFL_OUT_DQ":10,
            "PSL-PMC_MIXER_MON_OUT_DQ":10,
            "IMC-MCL_SERVO_OUT_DQ":30,
-           "PSL-PMC_TRANS_DC_OUT_DQ":30,
+           "PSL-PMC_TRANS_DC_OUT_DQ":35,
            "IMC-SERVO_SLOW_DAQ_OUT_DQ":7,
            "PEM-ACC_MCF_TABLE_REFL_Z_OUT_DQ":40,
            "PEM-ACC_PSL_PERI_PSL1_Y_OUT_DQ":20,
            "PEM-MIC_PSL_TABLE_PSL4_Z_OUT_DQ":20,
-           "LSC-REFL_PDA1_RF17_Q_ERR_DQ":7,
-           "LSC-POP_PDA1_RF17_Q_ERR_DQ":6,
-           "LSC-AS_PDA1_RF17_Q_ERR_DQ":10,
-           "LSC-REFL_PDA1_RF45_I_ERR_DQ":6,
-           "CAL-CS_PROC_IMC_FREQUENCY_DQ":6}
+           "LSC-REFL_PDA1_RF17_Q_ERR_DQ":16,
+           "LSC-REFL_PDA1_RF45_I_ERR_DQ":20,
+           "LSC-POP_PDA1_RF17_Q_ERR_DQ":18,
+           "LSC-POP_PDA1_DC_OUT_DQ":20,
+           "LSC-AS_PDA1_RF17_Q_ERR_DQ":25,
+           "CAL-CS_PROC_IMC_FREQUENCY_DQ":16}
 
 # get the time of the input file.
 tmp=inputfile.rsplit("-",2)
