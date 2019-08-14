74fefbbe81413012ded4fa7560488205ed2fa9cc
controls <controls@k1det0.kagra.icrr.u-tokyo.ac.jp>
2019-08-14 17:00:45 +0900

    Changed argument definition. ckozakai

--- iKozapy/Script/iKozapy.sh
+++ iKozapy/Script/iKozapy.sh
@@ -10,21 +10,23 @@
 ################################
 def_channel="K1:IMC-CAV_TRANS_OUT_DQ"
 #def_gpstime=`gpstime | grep GPS | awk '{printf("%d\n", $2-300)}'`
-def_gpsstart=`gpstime | grep GPS | awk '{printf("%d\n", $2-64)}'`
-def_gpsend=`gpstime | grep GPS | awk '{printf("%d\n", $2-32)}'`
 def_duration=32
-def_fres=1
 def_stride=1
-def_fft=1
+def_bandwidth=1
+def_outdir="/tmp/detchar/iKozapy/Result/"
+tmpoutdir="/users/DET/tools/iKozapy/Result/"
+fl=0
 
 while :
-do
-   
+do 
+    def_gpsstart=`gpstime | grep GPS | awk '{printf("%d\n", $2-64)}'`
+    def_gpsend=`gpstime | grep GPS | awk '{printf("%d\n", $2-32)}'`
+  
     flag=0
     while test ${flag} -eq 0
     do
-	[ -e /usr/bin/xclip ] && printf "${def_gpstime}" | xclip
-	ZEN_OUT=`zenity --forms --text="iKozapy for spectrogram${msg}" --separator=',' --add-entry="main channel (${def_channel})" --add-entry="gps start time (${def_gpsstart})" --add-entry="gps end time (${def_gpsend})" --add-entry="stride (${def_stride})"  --add-entry="FFT length[s] (${def_fft})" `
+	[ -e /usr/bin/xclip ] && printf "${def_gpsend}" | xclip
+	ZEN_OUT=`zenity --forms --text="iKozapy for spectrogram${msg}" --separator=',' --add-entry="main channel (${def_channel})" --add-entry="gps start time (${def_gpsstart})" --add-entry="gps end time (${def_gpsend})" --add-entry="Time resolution [sec] (${def_stride})"  --add-entry="Frequency resolution [Hz] (${def_bandwidth})" `
 	[ $? -eq 1 ] && exit 1
 	
 	#channel=`printf "${ZEN_OUT}" | cut -d',' -f1 | sed -e 's/K1://g'`
@@ -45,9 +47,9 @@ do
 	[ "${stride}" = "" ] && stride=${def_stride} || def_stride=${stride}
 	printf "stride: ${stride}\n" >&2
 	
-	fft=`printf "${ZEN_OUT}" | cut -d',' -f5`
-	[ "${fft}" = "" ] && fft=${def_fft} || def_fft=${fft}
-	printf "fft: ${fft}\n" >&2
+	bandwidth=`printf "${ZEN_OUT}" | cut -d',' -f5`
+	[ "${bandwidth}" = "" ] && bandwidth=${def_bandwidth} || def_bandwidth=${bandwidth}
+	printf "bandwidth: ${bandwidth}\n" >&2
 	
 	
 	[ "${channel}" = "" -a "${def_channel}" = "*" ] && msg=": Channel is not selected." && continue
@@ -55,15 +57,24 @@ do
 	flag=1
     done # end of while test flag
 
-    ssh -i /home/controls/.ssh/id_ed25519_detchar -o StrictHostKeyChecking=no -Y controls@k1det0 /users/DET/tools/GlitchPlot/Script/Kozapy/samples/run_whitening_spectrogram.sh -s ${gpsstart} -e ${gpsend} -c ${channel} --kamioka --stride ${stride} -f ${fft}  -o /users/DET/tools/iKozapy/Result/ > /users/DET/tools/iKozapy/Script/tmp 2>&1
+    fft=`echo "scale=5; 1. / $bandwidth" | bc `
 
-    output=$(tail -n 2 /users/DET/tools/iKozapy/Script/tmp | head -n 1) 
+    if [ ! -e ${def_outdir} ]; then
+	mkdir -p ${def_outdir}
+    fi
 
-    mkdir -p /tmp/detchar/iKozapy/Result
-    mv $output /tmp/detchar/iKozapy/Result/temp.png
+    ssh -i /home/controls/.ssh/id_ed25519_detchar -o StrictHostKeyChecking=no -Y controls@k1det0 /users/DET/tools/GlitchPlot/Script/Kozapy/samples/run_whitening_spectrogram.sh -s ${gpsstart} -e ${gpsend} -c ${channel} --kamioka --stride ${stride} -f ${fft}  -o ${tmpoutdir} --dpi 50 > ${def_outdir}/tmp 2>&1
 
-    eog /tmp/detchar/iKozapy/Result/temp.png &
-    #msg=": Done."
+    output=$(tail -n 2 ${def_outdir}/tmp | head -n 1) 
+
+    
+    mv $output ${def_outdir}/temp.png
+
+    if [ $fl = 0 ]; then
+	eog ${def_outdir}/temp.png &
+	fl=1
+    fi
+	#msg=": Done."
 
     #break
 done # end of while :
