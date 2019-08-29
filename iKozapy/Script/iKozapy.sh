d0c31ed34b5fd4010d2169383d4cbb1ed0a09105
controls <controls@k1sum1.kagra.icrr.u-tokyo.ac.jp>
2019-08-29 16:07:31 +0900

    Add coherencegram mode. ckozakai

--- iKozapy/Script/iKozapy.sh
+++ iKozapy/Script/iKozapy.sh
@@ -9,9 +9,11 @@
 ################################
 ### Set variable
 ################################
-def_channel="K1:IMC-CAV_TRANS_OUT_DQ"
+def_channel1="K1:IMC-CAV_TRANS_OUT_DQ"
+def_channel2=""
 #def_gpstime=`gpstime | grep GPS | awk '{printf("%d\n", $2-300)}'`
 def_realtime="n"
+def_mode="s"
 realtime=$def_realtime
 def_duration=32
 def_stride=1
@@ -20,7 +22,8 @@ def_minf=-1
 def_maxf=8000
 def_dpi=50
 def_outdir="/tmp/detchar/iKozapy/Result/"
-def_option=""
+def_options=""
+def_optionc=""
 tmpoutdir="/users/DET/tools/iKozapy/Result/"
 
 fl=0
@@ -34,48 +37,58 @@ do
     if [ "${realtime}" = "n" ]; then
 
 	[ -e /usr/bin/xclip ] && printf "${def_gpsend}" | xclip
-	ZEN_OUT=`zenity --forms --text="iKozapy for spectrogram${msg}" --separator=',' --add-entry="main channel (${def_channel})" --add-entry="JST end time (${def_gpsend})"  --add-entry="Real time update ? [y/n] (n)"  --add-entry="Duration [sec] (${def_duration})" --add-entry="Time resolution [sec] (${def_stride})"  --add-entry="Frequency resolution [Hz] (${def_bandwidth})" --add-entry="min. f [Hz] (autoscale)" --add-entry="max. f [Hz] (autoscale)" --add-entry="png resolution [dpi] (50)" --add-entry="Other option" `
+	ZEN_OUT=`zenity --forms --text="iKozapy plot tool" --separator=',' --add-entry="Spectrogram or coherencegram [s/c] (${def_mode})" --add-entry="channel1 (${def_channel1})" --add-entry="channel2 for coherencegram(${def_channel2})" --add-entry="JST or GPS end time (${def_gpsend})"  --add-entry="Real time update ? [y/n] (n)"  --add-entry="Duration [sec] (${def_duration})" --add-entry="Time resolution [sec] (${def_stride})"  --add-entry="Frequency resolution [Hz] (${def_bandwidth})" --add-entry="min. f [Hz] (autoscale)" --add-entry="max. f [Hz] (autoscale)" --add-entry="png resolution [dpi] (50)" --add-entry="Other option for spectrogram" --add-entry="Other option for coherencegram" `
 	#ZEN_OUT=`zenity --forms --text="iKozapy for spectrogram${msg}" --separator=',' --add-entry="main channel (${def_channel})" --add-entry="gps end time (${def_gpsend})"  --add-entry="Now ? [y/n] (n)"  --add-entry="Duration [sec] (${def_duration})" --add-entry="Time resolution [sec] (${def_stride})"  --add-entry="Frequency resolution [Hz] (${def_bandwidth})" --add-entry="min. f [Hz] (autoscale)" --add-entry="max. f [Hz] (autoscale)" --add-entry="png resolution [dpi] (50)" --add-entry="Other option" `
 	[ $? -eq 1 ] && exit 1
-	
+
+	mode=`printf "${ZEN_OUT}" | cut -d',' -f1 `
+	[ "${mode}" = ""  ] && mode=${def_mode}
+	[ "${mode}" != "" ] && def_mode=${mode}
+	printf "mode: ${mode} \n" >&2
+		
 	#channel=`printf "${ZEN_OUT}" | cut -d',' -f1 | sed -e 's/K1://g'`
-	channel=`printf "${ZEN_OUT}" | cut -d',' -f1 `
-	[ "${channel}" = ""  ] && channel=${def_channel}
-	[ "${channel}" != "" ] && def_channel=${channel}
-	printf "channel: ${channel} \n" >&2
+	channel1=`printf "${ZEN_OUT}" | cut -d',' -f2 `
+	[ "${channel1}" = ""  ] && channel1=${def_channel1}
+	[ "${channel1}" != "" ] && def_channel1=${channel1}
+	printf "channel1: ${channel1} \n" >&2
+		
+	channel2=`printf "${ZEN_OUT}" | cut -d',' -f3 `
+	[ "${channel2}" = ""  ] && channel2=${def_channel2}
+	[ "${channel2}" != "" ] && def_channel2=${channel2}
+	printf "channel2: ${channel2} \n" >&2
 		
 #	gpsstart=`printf "${ZEN_OUT}" | cut -d',' -f2`
 #	[ "${gpsstart}" = "" ] && gpsstart=${def_gpsstart} || def_gpsstart=${gpsstart}
 #	printf "gpsstart: ${gpsstart}\n" >&2
 	
-	gpsend=`printf "${ZEN_OUT}" | cut -d',' -f2`
+	gpsend=`printf "${ZEN_OUT}" | cut -d',' -f4`
 	[ "${gpsend}" = "" ] && gpsend=${def_gpsend} || def_gpsend=${gpsend}
 	gpsend=`${cmd_gps} $gpsend| grep GPS | awk '{printf("%d\n", $2)}'`
 	printf "gpsend: ${gpsend}\n" >&2
 
-	realtime=`printf "${ZEN_OUT}" | cut -d',' -f3`
+	realtime=`printf "${ZEN_OUT}" | cut -d',' -f5`
 	[ "${realtime}" = "" ] && realtime=${def_realtime} || def_realtime=${realtime}
 	printf "realtime: ${realtime}\n" >&2
 	
-	duration=`printf "${ZEN_OUT}" | cut -d',' -f4`
+	duration=`printf "${ZEN_OUT}" | cut -d',' -f6`
 	[ "${duration}" = "" ] && duration=${def_duration} || def_duration=${duration}
 	printf "duration: ${duration}\n" >&2
 
 	gpsstart=$(( $gpsend - $duration ))
 	
-	stride=`printf "${ZEN_OUT}" | cut -d',' -f5`
+	stride=`printf "${ZEN_OUT}" | cut -d',' -f7`
 	[ "${stride}" = "" ] && stride=${def_stride} || def_stride=${stride}
 	printf "stride: ${stride}\n" >&2
 	
-	bandwidth=`printf "${ZEN_OUT}" | cut -d',' -f6`
+	bandwidth=`printf "${ZEN_OUT}" | cut -d',' -f8`
 	[ "${bandwidth}" = "" ] && bandwidth=${def_bandwidth} || def_bandwidth=${bandwidth}
 	printf "bandwidth: ${bandwidth}\n" >&2
 	
-	minf=`printf "${ZEN_OUT}" | cut -d',' -f7`
+	minf=`printf "${ZEN_OUT}" | cut -d',' -f9`
 	[ "${minf}" = "" ] && minf=${def_minf} || def_minf=${minf}
 	printf "minf: ${minf}\n" >&2
 
-	maxf=`printf "${ZEN_OUT}" | cut -d',' -f8`
+	maxf=`printf "${ZEN_OUT}" | cut -d',' -f10`
 	[ "${maxf}" = "" ] && maxf=${def_maxf} || def_maxf=${maxf}
 	printf "maxf: ${maxf}\n" >&2
 		
@@ -83,40 +96,91 @@ do
 	[ "${dpi}" = "" ] && dpi=${def_dpi} || def_dpi=${dpi}
 	printf "dpi: ${dpi}\n" >&2
 		
-	option=`printf "${ZEN_OUT}" | cut -d',' -f10`
-	[ "${option}" = "" ] && option=${def_option} || def_option=${option}
-	printf "option: ${option}\n" >&2
+	options=`printf "${ZEN_OUT}" | cut -d',' -f10`
+	[ "${options}" = "" ] && options=${def_options} || def_options=${options}
+	printf "options: ${options}\n" >&2
 		
-	
-	[ "${channel}" = "" -a "${def_channel}" = "*" ] && msg=": Channel is not selected." && continue
-	#[ ${def_duration} -le 0 -o ${def_duration} -gt 900 ] && msg=": duration must ranges from 1 to 900s." && continue
+	optionc=`printf "${ZEN_OUT}" | cut -d',' -f11`
+	[ "${optionc}" = "" ] && optionc=${def_optionc} || def_optionc=${optionc}
+	printf "optionc: ${optionc}\n" >&2
+		
     fi
 
     if [ "${realtime}" = "y" ]; then
 	gpsend=`gpstime | grep GPS | awk '{printf("%d\n", $2)}'`
 	gpsstart=$(( $gpsend - $duration ))
     fi
+
     fft=`echo "scale=5; 1. / $bandwidth" | bc `
 
     if [ ! -e ${def_outdir} ]; then
 	mkdir -p ${def_outdir}
     fi
 
-    ssh -i /home/controls/.ssh/id_ed25519_detchar -o StrictHostKeyChecking=no -Y controls@k1det0 /users/DET/tools/GlitchPlot/Script/Kozapy/samples/run_whitening_spectrogram.sh -s ${gpsstart} -e ${gpsend} -c ${channel} --kamioka --stride ${stride} -f ${fft}  -o ${tmpoutdir} --dpi ${dpi} --fmin ${minf} --fmax ${maxf} ${option} > ${def_outdir}/tmp 2>&1
+    if [ "`echo $mode | grep s `" ]; then
+	ssh -i /home/controls/.ssh/id_ed25519_detchar -o StrictHostKeyChecking=no -Y controls@k1det0 /users/DET/tools/GlitchPlot/Script/Kozapy/samples/run_whitening_spectrogram.sh -s ${gpsstart} -e ${gpsend} -c ${channel1} --kamioka --stride ${stride} -f ${fft}  -o ${tmpoutdir} --dpi ${dpi} --fmin ${minf} --fmax ${maxf} ${option} > ${def_outdir}/tmp 2>&1
 
-    success=$(tail -n 1 ${def_outdir}/tmp)
-    if test "$success" = "Successfully finished !" ;then
-	output=$(tail -n 2 ${def_outdir}/tmp | head -n 1) 
+	success=$(tail -n 1 ${def_outdir}/tmp)
+	if test "$success" = "Successfully finished !" ;then
+	    output=$(tail -n 2 ${def_outdir}/tmp | head -n 1) 
 
-	echo $output
-	echo ${def_outdir}
-	mv $output ${def_outdir}/temp.png
-	sleep 1s    
-	eog ${def_outdir}/temp.png -w &
-    else
-	echo "job failes."
-	echo `tail  ${def_outdir}/tmp `
-    fi
+	    echo $output
+	    echo ${def_outdir}
+	    mv $output ${def_outdir}/temp_spectrogram1.png
+	    if [ -e ${output} ]; then
+		rm -rf ${output}
+	    fi    
+	    sleep 1s    
+	    eog ${def_outdir}/temp_spectrogram1.png -w &
+	else
+	    echo "job failes."
+	    echo `tail  ${def_outdir}/tmp `
+	fi
+
+#	if test "$channel2" != "" ;then
+#	    ssh -i /home/controls/.ssh/id_ed25519_detchar -o StrictHostKeyChecking=no -Y controls@k1det0 /users/DET/tools/GlitchPlot/Script/Kozapy/samples/run_whitening_spectrogram.sh -s ${gpsstart} -e ${gpsend} -c ${channel2} --kamioka --stride ${stride} -f ${fft}  -o ${tmpoutdir} --dpi ${dpi} --fmin ${minf} --fmax ${maxf} ${option} > ${def_outdir}/tmp 2>&1
+
+#	    success=$(tail -n 1 ${def_outdir}/tmp)
+#	    if test "$success" = "Successfully finished !" ;then
+#		output=$(tail -n 2 ${def_outdir}/tmp | head -n 1) 
+
+#		echo $output
+#		echo ${def_outdir}
+#		mv $output ${def_outdir}/temp_spectrogram2.png
+#		if [ -e ${output} ]; then
+#		    rm -rf ${output}
+#		fi    
+#		sleep 1s    
+#		eog ${def_outdir}/temp_spectrogram2.png -w &
+#	    else
+#		echo "job failes."
+#		echo `tail  ${def_outdir}/tmp `
+#	    fi
+	    
+#	fi #  if test "$channel2" != "" ;then
+    fi # if [ "`echo $mode | grep s `" ]; then
+
+    if [ "`echo $mode | grep c `" ]; then
+	ssh -i /home/controls/.ssh/id_ed25519_detchar -o StrictHostKeyChecking=no -Y controls@k1det0 /users/DET/tools/GlitchPlot/Script/Kozapy/samples/run_coherencegram.sh -s ${gpsstart} -e ${gpsend} -r ${channel1} -c ${channel2} --kamioka --stride ${stride} -f ${fft}  -o ${tmpoutdir} --dpi ${dpi} --fmin ${minf} --fmax ${maxf} ${option} > ${def_outdir}/tmp 2>&1
+
+	success=$(tail -n 1 ${def_outdir}/tmp)
+	if test "$success" = "Successfully finished !" ;then
+	    output=$(tail -n 2 ${def_outdir}/tmp | head -n 1) 
+
+	    echo $output
+	    echo ${def_outdir}
+	    mv $output ${def_outdir}/temp_coherencegram1.png
+	    if [ -e ${output} ]; then
+		rm -rf ${output}
+	    fi    
+	    sleep 1s    
+	    eog ${def_outdir}/temp_coherencegram1.png -w &
+	else
+	    echo "job failes."
+	    echo `tail  ${def_outdir}/tmp `
+	fi
+    fi # if [ "`echo $mode | grep c `" ]; then
+    
 done & # end of while :
 
 bg=$!
