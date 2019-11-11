856306c9c6942172051a7ae8c49815e699a2cf11
ckozakai <38804436+ckozakai@users.noreply.github.com>
2019-11-11 12:59:15 +0900

    Start suggestion script (coherence version)

---
+++ GlitchPlot/Script/suggestion.py
@@ -0,0 +1,8 @@
+'''
+This is to extract interesting channel in GlitchPlot. 
+The requirement is
+* The coherence is usually small
+* The coherence increases during glitch
+
+Author: Chihiro Kozakai
+'''
