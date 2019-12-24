f1d72d2a59a8c92f8bf9996d8c644bb175b12b43
ckozakai <38804436+ckozakai@users.noreply.github.com>
2019-12-24 12:23:57 +0900

    Update burst script for ER3 3 detector configuration.

--- GlitchPlot/Script/plotter_burst.sh
+++ GlitchPlot/Script/plotter_burst.sh
@@ -1,42 +1,51 @@
 #!/bin/bash
 
-infile=ER2_triggers_cWB.txt
+#infile=ER2_triggers_cWB.txt
+infile=burst/EVENTS_HVK.txt
 first=true
 outfile=parameter.txt
 
 rm -rf $outfile
 touch $outfile
 
-cat $infile | while read Index   Det     SNR             hrss            S_t                     C_t             C_f             BW              duration
+#cat $infile | while read Index   Det     SNR             hrss            S_t                     C_t             C_f             BW              duration
+cat $infile | while read passed dump rho cc01 cc2 cc3 camp tshift tsshift lh pf ed C_f BW duration npixel fres cwb tH tV tK SNRH SNRV SNRK hrssH hrssV hrssK phi theta psi
 do
-    if "$first"; then
-	first=false
+    #if "$first"; then
+	#first=false
+	#continue
+    #fi
+    if [ $passed = "#" ]; then
 	continue
     fi
 
-    if [ "$duration" = "" ]; then
-	continue
-    fi
+    #if [ "$duration" = "" ]; then
+#	continue
+    #fi
 
 
-    gpstime=`echo $S_t | sed -e 's/J1_//g'`
-    channel=K1:LSC-POP_PDA1_RF17_Q_ERR_DQ
+    #gpstime=`echo $S_t | sed -e 's/J1_//g'`
+    gpstime=$tK
+    #channel=K1:LSC-POP_PDA1_RF17_Q_ERR_DQ
+    channel=K1:CAL-CS_PROC_DARM_DISPLACEMENT_DQ
     min_duration=$duration
     max_duration=$duration
     bandwidth=$BW
-    maxSNR=$SNR
+    #maxSNR=$SNR
+    maxSNR=$SNRK
     frequency_SNR=$C_f
-    max_amp=$hrss
+    #max_amp=$hrss
+    max_amp=$hrssK
     frequency_amp=$C_f
     eventtype=Burst
     triggertype=cWB
 
-    if [ "$(echo "$maxSNR > 23" | bc)" -eq 1 ]; then
-	{
-	    echo $gpstime $channel $min_duration $max_duration $bandwidth $maxSNR $frequency_SNR $max_amp $frequency_amp $eventtype $triggertype $Index
-	    
-	} >> $outfile
-    fi
+#    if [ "$(echo "$maxSNR > 23" | bc)" -eq 1 ]; then
+    {
+	echo $gpstime $channel $min_duration $max_duration $bandwidth $maxSNR $frequency_SNR $max_amp $frequency_amp $eventtype $triggertype $Index
+	
+    } >> $outfile
+#    fi
 done
 
 echo $outfile
