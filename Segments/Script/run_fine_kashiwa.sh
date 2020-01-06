7969b5ec4d9e9d46668bcc5a0e018801e1f5a4f9
detchar <detchar@m31-01.kagra.icrr.u-tokyo.ac.jp>
2020-01-06 10:42:45 +0900

    Renamed script.

--- Segments/Script/run_fine_kashiwa.sh
+++ Segments/Script/run_fine_kashiwa.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #condor_submit /home/detchar/git/kagra-detchar/tools/Segments/Script/kashiwa_fine.sdf 
-python /home/detchar/git/kagra-detchar/tools/Segments/Script/test_division.py
+python /home/detchar/git/kagra-detchar/tools/Segments/Script/make15minSegment.py
