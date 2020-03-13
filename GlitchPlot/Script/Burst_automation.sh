843bb245cff78ca5dda3b271cf15b08c4acd9005
ckozakai <38804436+ckozakai@users.noreply.github.com>
2020-03-13 18:11:22 +0900

    Burst automation code send mail when finished.

--- GlitchPlot/Script/Burst_automation.sh
+++ GlitchPlot/Script/Burst_automation.sh
@@ -6,6 +6,7 @@ gpstime
 
 startdate=`date -d '-9 day' +%Y-%m-%d`
 enddate=`date -d '-3 day' +%Y-%m-%d`
+enddateJST=`date -d '-2 day' +%Y-%m-%d`
 starttime=`tconvert $startdate 8:00:00`
 endtime=`tconvert $enddate 23:59:59`
 
@@ -20,7 +21,7 @@ echo $endtime
 
 while :
 do
-    sleep 60
+    #sleep 60
     echo "check condor."
     tmp=`condor_q | grep chihiro`
     if [ "${tmp}" = "" ]; then
@@ -29,3 +30,19 @@ do
         break;
     fi
 done
+
+#cat mail.txt | sendmail -i -t
+
+{
+    echo "From: ckozakai <ckozakai@icrr.u-tokyo.ac.jp>"
+    echo "To: ckozakai <ckozakai@icrr.u-tokyo.ac.jp>"
+    echo "Subject: Please reprocess Yuzu summary page"
+    echo ""
+    echo "Yuzurihara-sama,"
+    echo ""
+    echo "Please reprocess Yuzu summary page during last week."
+    echo "From " $startdate " to " $enddateJST "."
+    echo ""
+    echo "Best,"
+    echo "Chihiro Kozakai"
+} | sendmail -i -t
