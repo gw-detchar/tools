afd8941a99722bd2cd523e158e819f3830e6e681
controls <controls@k1sum1.kagra.icrr.u-tokyo.ac.jp>
2020-04-28 20:02:38 +0900

    script for manual run.

---
+++ Omicron/Script/manualrun.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+/users/DET/tools/Omicron/Script/manual_segment.sh /users/DET/tools/Omicron/Parameter/summary_4096.txt /users/DET/Segments/K1-GRD_LOCKED/2020/K1-GRD_LOCKED_SEGMENT_UTC_2020-04-14.txt
+/users/DET/tools/Omicron/Script/manual_segment.sh /users/DET/tools/Omicron/Parameter/summary_4096.txt /users/DET/Segments/K1-GRD_LOCKED/2020/K1-GRD_LOCKED_SEGMENT_UTC_2020-04-15.txt
+/users/DET/tools/Omicron/Script/manual_segment.sh /users/DET/tools/Omicron/Parameter/summary_4096.txt /users/DET/Segments/K1-GRD_LOCKED/2020/K1-GRD_LOCKED_SEGMENT_UTC_2020-04-16.txt
+/users/DET/tools/Omicron/Script/manual_segment.sh /users/DET/tools/Omicron/Parameter/summary_4096.txt /users/DET/Segments/K1-GRD_LOCKED/2020/K1-GRD_LOCKED_SEGMENT_UTC_2020-04-17.txt
+/users/DET/tools/Omicron/Script/manual_segment.sh /users/DET/tools/Omicron/Parameter/summary_4096.txt /users/DET/Segments/K1-GRD_LOCKED/2020/K1-GRD_LOCKED_SEGMENT_UTC_2020-04-18.txt
+/users/DET/tools/Omicron/Script/manual_segment.sh /users/DET/tools/Omicron/Parameter/summary_4096.txt /users/DET/Segments/K1-GRD_LOCKED/2020/K1-GRD_LOCKED_SEGMENT_UTC_2020-04-19.txt
+/users/DET/tools/Omicron/Script/manual_segment.sh /users/DET/tools/Omicron/Parameter/summary_4096.txt /users/DET/Segments/K1-GRD_LOCKED/2020/K1-GRD_LOCKED_SEGMENT_UTC_2020-04-20.txt
+/users/DET/tools/Omicron/Script/manual_segment.sh /users/DET/tools/Omicron/Parameter/summary_4096.txt /users/DET/Segments/K1-GRD_LOCKED/2020/K1-GRD_LOCKED_SEGMENT_UTC_2020-04-21.txt
+
