829bb9702ecbc6ce3cc73d0bb06a885a7f57f88c
controls <controls@k1sum1.kagra.icrr.u-tokyo.ac.jp>
2020-05-21 20:00:24 +0900

    Update.

--- GlitchPlot/Script/checkOmicron.py
+++ GlitchPlot/Script/checkOmicron.py
@@ -17,66 +17,70 @@ gpsend = locked.known[0][1]
 #omicron = DataQualityFlag(name="Omicron",known = [(gpsstart,gpsend)])
 
 channels = []
-with open("/users/DET/tools/Omicron/Parameter/O3rerun_1024.txt") as f:
+with open("/users/DET/tools/Omicron/Parameter/O3rerun_512.txt") as f:
     lines = f.readlines()
     for line in lines:
         if "DATA CHANNELS " in line:
             channel=line.split()[-1]
             channels.extend([channel])
 
+for channel in channels:
 
+    tmpchannel = channel.replace("K1:","")
+    tmpchannel = tmpchannel.replace("-","_")
+    files = []
 
-files = []
+    for i in range(int(gpsstart/100000),int(gpsend/100000)+1):
+        #tmp = glob.glob("/home/controls/triggers/K1/CAL_CS_PROC_DARM_DISPLACEMENT_DQ_OMICRON/"+str(i)+"/K1-CAL_CS_PROC_DARM_DISPLACEMENT_DQ_OMICRON-*")
+        tmp = glob.glob("/home/controls/triggers/K1/"+tmpchannel+"_OMICRON/"+str(i)+"/*")
+        files.extend(tmp)
 
-for i in range(int(gpsstart/100000),int(gpsend/100000)+1):
-    #tmp = glob.glob("/home/controls/triggers/K1/CAL_CS_PROC_DARM_DISPLACEMENT_DQ_OMICRON/"+str(i)+"/K1-CAL_CS_PROC_DARM_DISPLACEMENT_DQ_OMICRON-*")
-    tmp = glob.glob("/home/controls/triggers/K1/*_OMICRON/"+str(i)+"/*")
-    files.extend(tmp)
+    files.sort()
+    
+    for fname in files:
+        start = int(fname.rsplit('-',2)[1])
+        duration = fname.rsplit('-',2)[2]
+        duration = int(duration.split('.')[0])
+        tmp = DataQualityFlag(known = omicron.known, active = [(start, start+duration)])
+        omicron += tmp
 
-files.sort()
+    #=============Get omicron failed segments=============
 
-for fname in files:
-    start = int(fname.rsplit('-',2)[1])
-    duration = fname.rsplit('-',2)[2]
-    duration = int(duration.split('.')[0])
-    tmp = DataQualityFlag(known = omicron.known, active = [(start, start+duration)])
-    omicron += tmp
+    failed = DataQualityFlag(name="Failed",known = [(gpsstart,gpsend)])
 
-#=============Get omicron failed segments=============
+    #with open("/users/DET/tools/Omicron/Script/log200428.dat") as f:  # for 4/14-20
+    #with open("/users/DET/tools/Omicron/Script/log200519_4096.dat") as f:  # for 4/14-20
+    with open("/users/DET/tools/Omicron/Script/log200519_256-1024.dat") as f:  # for 4/14-20
+    #with open("/users/DET/tools/Omicron/Script/log200504.dat") as f:  # for 4/7-12
+        lines = f.readlines()
+        for line in lines:
+            if "Omicron::ExtractTriggers: the maximum trigger rate (5000.00000 Hz) is exceeded ("+channel+" " in line:
+                time=line.split()[-1]
+                tmpstart = int(time.split("-")[0])
+                tmpend = int(time.split("-")[1][0:10])
+                tmp = DataQualityFlag(known = failed.known, active = [(tmpstart, tmpend)])
+                failed += tmp
 
-failed = DataQualityFlag(name="Failed",known = [(gpsstart,gpsend)])
+    unknown = locked - omicron
+    unknown -= failed
 
-#with open("/users/DET/tools/Omicron/Script/log200428.dat") as f:  # for 4/14-20
-with open("/users/DET/tools/Omicron/Script/log200519_4096.dat") as f:  # for 4/14-20
-#with open("/users/DET/tools/Omicron/Script/log200504.dat") as f:  # for 4/7-12
-    lines = f.readlines()
-    for line in lines:
-        if "Omicron::ExtractTriggers: the maximum trigger rate (5000.00000 Hz) is exceeded (K1:CAL-CS_PROC_DARM_DISPLACEMENT_DQ " in line:
-            time=line.split()[-1]
-            tmpstart = int(time.split("-")[0])
-            tmpend = int(time.split("-")[1][0:10])
-            tmp = DataQualityFlag(known = failed.known, active = [(tmpstart, tmpend)])
-            failed += tmp
+    print(channel)
+    print("Locked segments")
+    print(locked)
+    print("Omicron succeeded")
+    print(omicron)
+    print("Omicron failed")
+    print(failed)
 
-unknown = locked - omicron
-unknown -= failed
+    print("Unknown segments")
+    print(unknown)
 
-print("Locked segments")
-print(locked)
-print("Omicron succeeded")
-print(omicron)
-print("Omicron failed")
-print(failed)
-
-print("Unknown segments")
-print(unknown)
-
-with open("unknownSegments0519_1024.txt", mode='w') as f:
-    for seg in unknown.active :
-        f.write('{0} {1}\n'.format(int(seg[0]), int(seg[1])))
-with open("failedSegments0519_1024.txt", mode='w') as f:
-    for seg in failed.active :
-        f.write('{0} {1}\n'.format(int(seg[0]), int(seg[1])))
-with open("succeededSegments0519_1024.txt", mode='w') as f:
-    for seg in omicron.active :
-        f.write('{0} {1}\n'.format(int(seg[0]), int(seg[1])))
+    with open("unknownSegments0519_512_"+channel+".txt", mode='w') as f:
+        for seg in unknown.active :
+            f.write('{0} {1}\n'.format(int(seg[0]), int(seg[1])))
+    with open("failedSegments0519_512_"+channel+".txt", mode='w') as f:
+        for seg in failed.active :
+            f.write('{0} {1}\n'.format(int(seg[0]), int(seg[1])))
+    with open("succeededSegments0519_512_"+channel+".txt", mode='w') as f:
+        for seg in omicron.active :
+            f.write('{0} {1}\n'.format(int(seg[0]), int(seg[1])))
--- Omicron/Script/manualrun.sh
+++ Omicron/Script/manualrun.sh
@@ -8,4 +8,9 @@
 #/users/DET/tools/Omicron/Script/manual_segment.sh /users/DET/tools/Omicron/Parameter/O3rerun_4096.txt /users/DET/Segments/K1-GRD_LOCKED/2020/K1-GRD_LOCKED_SEGMENT_UTC_2020-04-11.txt
 #/users/DET/tools/Omicron/Script/manual_segment.sh /users/DET/tools/Omicron/Parameter/O3rerun_4096.txt /users/DET/Segments/K1-GRD_LOCKED/2020/K1-GRD_LOCKED_SEGMENT_UTC_2020-04-12.txt
 #/users/DET/tools/Omicron/Script/manual_segment.sh /users/DET/tools/Omicron/Parameter/O3rerun_4096.txt /users/DET/Segments/K1-GRD_LOCKED/2020/K1-GRD_LOCKED_SEGMENT_UTC_2020-04-13.txt
-/users/DET/tools/Omicron/Script/manual_segment.sh /users/DET/tools/Omicron/Parameter/O3rerun_4096.txt /users/DET/tools/Segments/Script/science_SEGMENT_UTC_2020-04-15.txt
+
+#/users/DET/tools/Omicron/Script/manual_segment.sh /users/DET/tools/Omicron/Parameter/O3rerun_4096.txt /users/DET/Segments/K1-GRD_LOCK_STATE_N_EQ_1000/2020/K1-GRD_LOCK_STATE_N_EQ_1000_SEGMENT_UTC_2020-04-15.txt
+#/users/DET/tools/Omicron/Script/manual_segment.sh /users/DET/tools/Omicron/Parameter/O3rerun_1024.txt /users/DET/Segments/K1-GRD_LOCK_STATE_N_EQ_1000/2020/K1-GRD_LOCK_STATE_N_EQ_1000_SEGMENT_UTC_2020-04-15.txt
+#/users/DET/tools/Omicron/Script/manual_segment.sh /users/DET/tools/Omicron/Parameter/O3rerun_256.txt /users/DET/Segments/K1-GRD_LOCK_STATE_N_EQ_1000/2020/K1-GRD_LOCK_STATE_N_EQ_1000_SEGMENT_UTC_2020-04-15.txt
+#/users/DET/tools/Omicron/Script/manual_segment.sh /users/DET/tools/Omicron/Parameter/O3rerun_512.txt /users/DET/Segments/K1-GRD_LOCK_STATE_N_EQ_1000/2020/K1-GRD_LOCK_STATE_N_EQ_1000_SEGMENT_UTC_2020-04-15.txt
+/users/DET/tools/Omicron/Script/manual_segment.sh /users/DET/tools/Omicron/Parameter/O3rerun_2048.txt /users/DET/Segments/K1-GRD_LOCK_STATE_N_EQ_1000/2020/K1-GRD_LOCK_STATE_N_EQ_1000_SEGMENT_UTC_2020-04-15.txt
