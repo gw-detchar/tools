d6bddb380f8037f0e70f55305c6b9aee4e10db0a
controls <controls@k1det1.kagra.icrr.u-tokyo.ac.jp>
2025-01-08 16:24:46 +0900

    Updated the script for ndscope (YamaT)

--- ndscope/Script/gui4medm.sh
+++ ndscope/Script/gui4medm.sh
@@ -2,41 +2,64 @@
 #******************************************#
 #     File Name: ndscope/gui4medm.sh
 #        Author: Takahiro Yamamoto
-# Last Modified: 2019/01/27 17:47:59
+# Last Modified: 2022/09/13 16:06:57
 #******************************************#
 
 ################################
 ### Set variable
 ################################
+NDS2SERVER=k1nds2:8088
+#NDSSERVER=157.82.231.173:31200
 CMD_ndscope=/usr/bin/ndscope
+INI=/opt/rtcds/kamioka/k1/chans/daq/K1EDCU_GRD.ini
+GRD="`grep STATE_N ${INI} | tr -d ']' | tr -d '[' | tr '\n' '|'`"
 
 ################################
 ### Set variable
 ################################
 def_gpsend=`gpstime | grep GPS | awk '{printf("%d\n", $2-64)}'`
+def_dateend=`gpstime ${def_gpsend} -u -f "%Y-%m-%d"`
+def_timeend=`gpstime ${def_gpsend} -u -f "%H:%M:%S"`
 def_duration=86400
 def_unit=seconds
-
+echo "${GRD}"
 ################################
 ### GUI interface
 ################################
 flag=0
 while test ${flag} -eq 0
 do
-    ZEN_OUT=`zenity --forms --text="ndscope${msg}" --separator=',' --add-entry="gps end (${def_gpsend})" --add-entry="duration (${def_duration})" --add-combo="unit (${def_unit})" --combo-values='seconds|minutes|hours|days' 2> /dev/null`
+    ZEN_OUT=`zenity --forms --text="ndscope${msg}" --separator=',' --add-entry="gps end (${def_gpsend})" --add-entry="date (${def_dateend})" --add-entry="time (${def_timeend})" --add-entry="duration (${def_duration})" --add-combo="unit (${def_unit})" --combo-values='seconds|minutes|hours|days' --add-combo="grd state (NONE)" --combo-values="${GRD}" 2> /dev/null`
     [ $? -eq 1 ] && exit 1
     
     gpsend=`printf "${ZEN_OUT}" | cut -d',' -f1`
     [ "${gpsend}" = "" ] && gpsend=${def_gpsend} || def_gpsend=${gpsend}
     printf "gpsend: ${gpsend}\n" >&2 
     
-    duration=`printf "${ZEN_OUT}" | cut -d',' -f2`
+    dateend=`printf "${ZEN_OUT}" | cut -d',' -f2`
+    [ "${dateend}" = "" ] && dateend=${def_dateend} #|| def_dateend=${dateend}
+    printf "gpsend: ${dateend}\n" >&2 
+    
+    timeend=`printf "${ZEN_OUT}" | cut -d',' -f3`
+    [ "${timeend}" = "" ] && timeend=${def_timeend} #|| def_timeend=${timeend}
+    printf "gpsend: ${timeend}\n" >&2 
+    
+    duration=`printf "${ZEN_OUT}" | cut -d',' -f4`
     [ "${duration}" = "" ] && duration=${def_duration} || def_duration=${duration}
     printf "duration: ${duration}\n" >&2
 
-    unit=`printf "${ZEN_OUT}" | cut -d',' -f3`
+    unit=`printf "${ZEN_OUT}" | cut -d',' -f5`
     [ "${unit}" = "" ] && unit=${def_unit} || def_unit=${unit}
     printf "unit: ${unit}\n" >&2
+
+    grd=`printf "${ZEN_OUT}" | cut -d',' -f6`
+    printf "grd: ${grd}\n" >&2
+
+    if test ${dateend} != ${def_dateend} -o ${timeend} != ${def_timeend}
+    then
+	gpsend=`gpstime "${dateend} ${timeend} UTC" | grep GPS | awk '{printf("%d\n", $2-64)}'`
+	printf "gpsend: ${gpsend}\n" >&2
+    fi
 
     case "${unit}" in
 	"seconds") duration=${duration};;
@@ -49,5 +72,6 @@ do
     flag=1
 done # end of while test flag
 
+CHANS="`echo $@ | tr ' ' '\n' | sort | uniq | tr '\n' ' ' | sed -e 's/SW1R/SWSTAT/g' -e 's/SW2R/SWSTAT/g' -e 's/_OUTMON/_OUT16/g'`"
+${CMD_ndscope} --nds ${NDS2SERVER} -t ${gpsepoch} -w ${duration} ${CHANS} ${grd}
 
-${CMD_ndscope} -t ${gpsepoch} -w ${duration} $1
--- ndscope/Script/ndscope_fast.sh
+++ ndscope/Script/ndscope_fast.sh
@@ -2,34 +2,108 @@
 #******************************************#
 #     File Name: ndscope/ndscope_fast.sh
 #        Author: Takahiro Yamamoto
-# Last Modified: 2019/08/11 18:19:38
+# Last Modified: 2023/08/04 18:48:21
 #******************************************#
 
-BIN=/usr/bin/ndscope
-CHANS=/opt/rtcds/kamioka/k1/chans/daq/
+############################################
+###  Environment
+############################################
+BIN="/usr/bin/ndscope"
+MASTER="/opt/rtcds/kamioka/k1/target/fb/master"
 
-[ "$1" = "" ] && echo "Usage: $0 channel" && exit 1
+############################################
+###  Argcheck
+############################################
+if test "$1" = ""
+then
+    echo "Usage: $0 [slow] channel1 [channel2 [channel3 ...]]" 
+    exit 1
+elif test "$1" = "slow"
+then
+    slow=1
+    opt_w="-w 90"
+    INCHS=`echo "${@:2}" | tr ' ' '\n' | sort | uniq | tr '\n' ' '`
+else
+    slow=0
+    opt_w="-w 10"
+    INCHS=`echo "$@" | tr ' ' '\n' | sort | uniq | tr '\n' ' '`
+fi
 
-INCHS=`echo "$@" | tr ' ' '\n' | sort | uniq | tr '\n' ' '`
-for x in ${INCHS}
+############################################
+###  Argcheck
+############################################
+CHANS="`grep ini$ ${MASTER} | grep -v '^#'`"
+for INCH in ${INCHS}
 do
-    INCH=$x
-
-    if test "`grep -h ${INCH%*_MON}_DQ ${CHANS}*.ini`"
+    ### convert SDF list to channel name
+    if test "`printf "${INCH}" | grep "_SDF_SP_STAT"`" != ""
     then
-	INCH=${INCH%*_MON}
+    	tmp=`printf "${INCH}" | sed -e 's/_BURT//g' -e 's/_LIVE//g' -e 's/_DIFF//g' -e 's/_TIME//g'`
+    	INCH=`caget -S ${tmp} | awk '{print $2}'`
+    	caget -S ${INCH} > /dev/null 2>&1
+    	ret=$?
+    	if test $ret -ne 0
+    	then
+    	    INCH=`caget -S ${tmp} | awk '{print $2}'`_SWSTAT
+    	fi
     fi
-    
 
-    OUTCH=`printf ${INCH} | sed -e 's/I_MON/I_ERR/g' -e 's/Q_MON/Q_ERR/g' -e 's/OUT16/OUT/g' -e 's/OUTMON/OUT/g' -e 's/OUTPUT/OUT/g' -e 's/INMON/IN1/g' -e 's/_EPICS_CH/_TP_CH/g'`
+    ### convert non-DAQ-ed EPICS to DAQ-ed EPICS (slow)
+    OUTCH=`printf ${INCH} \
+          | sed -e 's/_OUTMON/_OUTPUT/g' -e 's/_SW1R/_SWSTAT/g' -e 's/_SW2R/_SWSTAT/g' \
+	        -e 's/_SW1/_SWSTAT/g' -e 's/_SW2/_SWSTAT/g' -e 's/_Name0[0-9]/_SWSTAT/g' \
+		-e 's/_STATE_WORD$/_STATE_WORD_FE/g'`
 
-    if test "`grep -h ${OUTCH}_DQ ${CHANS}*.ini | grep -v '^#'`" = ""
+    if test ${slow} -eq 0
     then
-	:
-    else
-	OUTCH=${OUTCH}_DQ
+	### convert EPICS to TP
+	tmp=${OUTCH}
+	OUTCH=`printf ${tmp} \
+	      | sed -e 's/_\([IQ]\)_MON$/_\1_ERR/g' \
+	            -e 's/_INMON$/_IN1/g' -e 's/_EXCMON$/_EXC/g' \
+		    -e 's/_OUT16$/_OUT/g' -e 's/_OUTPUT$/_OUT/g' \
+		    -e 's/_WIT_\([LTVRPY]\)MON$/_WIT_\1/g' \
+		    -e 's/_LINEMON$/LINE_OUT/g' -e 's/_SLOW$//g' \
+		    -e 's/_EPICS_CH/_TP_CH/g'`
+	
+	### convert DAC_OUTPUT EPICS to TP
+	tmp=${OUTCH}
+	if test "`printf ${tmp} | grep 'FEC-.*_DAC_OUTPUT_'`" != ""
+	then
+	    fec=`printf ${tmp} | awk -F'[-_]' '{print $2}'`
+	    dac=`printf ${tmp} | awk -F'_' '{print $4}'`
+	    ch=`printf ${tmp} | awk -F'_' '{print $5}'`
+	    mdl=`grep -l ${tmp} $CHANS | grep -v '^#'`
+	    sys=`basename ${mdl} .ini | cut -c 3-5`
+	    name=`basename ${mdl} .ini | cut -c 6-`
+	    OUTCH="K1:${sys}-${name}_MDAC${dac}_TP_CH${ch}"
+	fi
+
+	### convert MON
+	tmp=${OUTCH%*_MON}
+	if test "`grep -h "${tmp}" ${CHANS} | grep -v '^#'`" != ""
+	then
+	    OUTCH=${tmp}
+	fi
+
+	### convert TP to DQ if exist
+	if test "`grep -h ${OUTCH}_DQ ${CHANS} | grep -v '^#'`" != ""
+	then
+	    OUTCH=${OUTCH}_DQ
+	elif test "`grep -h ${OUTCH}_DBL_DQ ${CHANS} | grep -v '^#'`" != ""
+	then
+	    OUTCH=${OUTCH}_DBL_DQ
+	fi
+    fi
+
+    ### convert Ramp matrix
+    if test "`printf "${OUTCH}" | grep MTRX_RAMPING`" != ""
+    then
+	tmp=${OUTCH}
+	OUTCH="`printf "${tmp}" | sed -e 's/_RAMPING//g'` `printf "${tmp}" | sed -e 's/_RAMPING/_SETTING/g'` ${tmp}"
     fi
     
     OUTCHS="${OUTCHS} ${OUTCH}"
 done
-${BIN} ${OUTCHS}
+
+${BIN} ${opt_w} ${OUTCHS}
