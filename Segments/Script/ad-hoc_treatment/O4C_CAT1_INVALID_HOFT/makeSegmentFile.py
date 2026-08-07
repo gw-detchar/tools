60b6bbf827c74918030447e04e0a87b8d22faa03
yamat <yamat@icrr.u-tokyo.ac.jp>
2026-08-07 10:52:39 +0900

    minor update about HOFT_NOT_OK segments
    unify segment name and file path for releasing it on the shared space

--- Segments/Script/ad-hoc_treatment/O4C_CAT1_INVALID_HOFT/makeSegmentFile.py
+++ Segments/Script/ad-hoc_treatment/O4C_CAT1_INVALID_HOFT/makeSegmentFile.py
@@ -2,19 +2,19 @@
 #******************************************#
 #     File Name: makeSegmentFile.py
 #        Author: Takahiro Yamamoto
-# Last Modified: 2026/07/28 14:20:32
+# Last Modified: 2026/08/07 10:47:27
 #******************************************#
 
 import os
 from gwpy.segments import DataQualityFlag,SegmentList
 
 def INVALID_MEASUREMENT():
-    segname = 'CAL_HOFT_NOT_OK_INVALID_MEASUREMENT'
+    segname = 'CAL_HOFT_NOT_OK'
     DQF = DataQualityFlag(
         name = 'K1:{0}:1'.format(segname),
         category = None,
         label = 'INVALID_HOFT',
-        description = 'hoft is unavailable due to invalid CAL measurements',
+        description = 'hoft is unavailable due to calibration issue',
         isgood = False,
     )
 
@@ -38,7 +38,7 @@ if __name__ == '__main__':
 
     DQF = INVALID_MEASUREMENT()
     os.makedirs(args.path, exist_ok=True)
-    DQF.write('{0}/K1-CAL_HOFT_NOT_OK_O4C.xml'.format(args.path), format='ligolw')
+    DQF.write('{0}/K1-CAL_HOFT_NOT_OK_O4c.xml'.format(args.path), format='ligolw')
 
 ############################################
 ### EOF
